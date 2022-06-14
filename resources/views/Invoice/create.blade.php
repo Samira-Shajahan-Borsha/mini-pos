9a7088ef9231b768d0c050f49def58870ee30f89
Samira Shajahan Borsha <samiraborsha08@gmail.com>
2022-06-14 12:40:10 +0600

    form design modified

--- resources/views/Invoice/create.blade.php
+++ resources/views/Invoice/create.blade.php
@@ -1,46 +1,39 @@
 @extends('Include.layout')
 
 @section('content')
-<h1 class="container">Create Invoice</h1><br>
-<form action="" method="POST" class="container">
-    @csrf
-    <div>
-        <label for="invoice_num" class="text-success fs-5 fw-semibold">Invoice Number: 
-        <input type="number" name="invoice_num" id="" placeholder="Invoice Number" >
-{{--         <div class="text-danger">{{ $errors->first('name') }}</div>
- --}}    </label>
-    </div><br>
-    <div>
-        <label for="total" class="text-success fs-5 fw-semibold">Total: 
-        <input type="number" name="total" id="" placeholder="Total">
-{{--         <div class="text-danger">{{ $errors->first('sku') }}</div>
- --}}    </label>
-    </div><br>
-    <div>
-        <label for="payment_method" class="text-success fs-5 fw-semibold">Payment Method: 
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-         </label>
-    </div><br>
-    <div>
-        <label for="name" class="text-success fs-5 fw-semibold">Date:
-        <input type="text" placeholder="Date" name="available_quantity">
-{{--         <div class="text-danger">{{ $errors->first('available_quantity') }}</div>
- --}}    </label>
-    </div><br>
-    <div>
-        <label for="name" class="text-success  fs-5 fw-semibold">Purchase Price: 
-        <input type="text" placeholder="Purchase Price" name="purchase_price">
-{{--         <div class="text-danger">{{ $errors->first('purchase_price') }}</div>
- --}}
-        </label>
-    </div><br>
-    <button type="submit" class="btn btn-primary  fs-5 fw-semibold">Add</button>
-</form>
+<h2 class="text-center">Create Invoice</h2><br>
+<form class="row g-3" action="" method="">
+  @csrf
+    <div class="col-md-6">
+      <label for="name" class="form-label fs-5 fw-semibold">Invoice Number</label>
+      <input type="number" class="form-control" id="name" name="invoice_num" placeholder="Enter Invoice Number">
+    </div>
+    
+    <div class="col-md-6">
+      <label for="inputState" class="form-label fs-5 fw-semibold">Payment Method</label>
+      <select id="inputState" class="form-select">
+        <option selected>Cash</option>
+        <option>Bkash</option> 
+        <option>Card</option> 
+      </select>
+    </div>
+    <div class="col-md-6">
+        <label for="customerEmail" class="form-label fs-5 fw-semibold">Customer Email</label>
+        <input type="text" class="form-control" id="customerEmail" name="customer_email" placeholder="Enter customer mail">
+    </div>
+    <div class="col-md-6">
+        <label for="inputDate" class="form-label fs-5 fw-semibold">Date</label>
+        <div class="row">
+            <div class="col-md-12">
+                <input type="date" id="date" name="date" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center">
+      <button type="submit" class="btn btn-primary">Create Invoice</button>
+    </div>
+  </form>
+
 @endsection
 
 
--- resources/views/Product/create.blade.php
+++ resources/views/Product/create.blade.php
@@ -1,41 +1,37 @@
 @extends('Include.layout')
 
 @section('content')
-    <h1 class="container">Add product</h1><br>
-    <form action="{{ route('product.store') }}" method="POST" class="container">
+    <h2 class="text-center">Add Product</h2><br>
+    <form class="row g-3" action="{{ route('product.store') }}" method="POST">
         @csrf
-        <div>
-            <label for="name" class="text-success fs-5 fw-semibold">Product Name: 
-            <input type="text" placeholder="Product Name" name="name">
-            <div class="text-danger">{{ $errors->first('name') }}</div>
-        </label>
-        </div><br>
-        <div>
-            <label for="name" class="text-success fs-5 fw-semibold">SKU: 
-            <input type="text" placeholder="SKU" name="sku">
-            <div class="text-danger">{{ $errors->first('sku') }}</div>
-        </label>
-        </div><br>
-        <div>
-            <label for="name" class="text-success fs-5 fw-semibold">Description: 
-            <input type="text" placeholder="Description" name="description">
+        <div class="col-md-6">
+          <label for="name" class="form-label fs-5 fw-semibold">Product Name</label>
+          <input type="text" class="form-control" id="name" name="name" placeholder="Enter product name">
+          <div class="text-danger">{{ $errors->first('name') }}</div>
+        </div>
+        <div class="col-md-6">
+          <label for="sku" class="form-label fs-5 fw-semibold">SKU</label>
+          <input type="text" class="form-control" id="sku" name="sku" placeholder="Enter SKU">
+          <div class="text-danger">{{ $errors->first('sku') }}</div>
+        </div>
+        
+        <div class="col-md-6">
+            <label for="description" class="form-label fs-5 fw-semibold">Description</label>
+            <input type="text" class="form-control" id="description" name="description" placeholder="Description">
             <div class="text-danger">{{ $errors->first('description') }}</div>
-        </label>
-        </div><br>
-        <div>
-            <label for="name" class="text-success fs-5 fw-semibold">Available Quantity:
-            <input type="text" placeholder="Available Quantity" name="available_quantity">
+        </div>
+        <div class="col-md-6">
+            <label for="available_quantity" class="form-label fs-5 fw-semibold">Available Quantity</label>
+            <input type="number" class="form-control" id="available_quantity" name="available_quantity" placeholder="Enter available quantity">
             <div class="text-danger">{{ $errors->first('available_quantity') }}</div>
-        </label>
-        </div><br>
-        <div>
-            <label for="name" class="text-success  fs-5 fw-semibold">Purchase Price: 
-            <input type="text" placeholder="Purchase Price" name="purchase_price">
+        </div>
+        <div class="col-md-6">
+            <label for="available_quantity" class="form-label fs-5 fw-semibold">Purchase Price</label>
+            <input type="number" class="form-control" id="purchase_price" name="purchase_price" placeholder="Enter purchase price">
             <div class="text-danger">{{ $errors->first('purchase_price') }}</div>
-
-            </label>
-        </div><br>
-        <button type="submit" class="btn btn-primary  fs-5 fw-semibold">Add</button>
-    </form>
-
+        </div>
+        <div class="col-12 text-center">
+          <button type="submit" class="btn btn-primary">Add</button>
+        </div>
+      </form>
 @endsection
